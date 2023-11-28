7d125c0b159db4fe549df6a377385b5e1a45979e
PutraZakaria <117093982+PutraZakaria@users.noreply.github.com>
2023-11-28 20:41:50 +0700

    database

--- database/dummy.sql
+++ database/dummy.sql
@@ -16,15 +16,15 @@ INSERT INTO Kategori (ID_Kategori, Nama) VALUES
 	('K2', 'Elektronik'),
 	('K3', 'ATK');
 
-INSERT INTO Pengguna (ID_Pengguna, ID_Level, Username, Password, Nama, Email, Nomor_HP, Status) VALUES
-	('P1', 'L2', '404079101', 'adei', 'Ade Ismail', 'AdeIsmail@polinema.ac.id', '82182', 'AKTIF'),
-	('P2', 'L3', '2241720220', 'putraz', 'Putra Zakaria', 'PutraZakaria@polinema.ac.id', '82183', 'TIDAK AKTIF'),
-	('P3', 'L2', '702108601', 'eloks', 'Elok Nur Hamdana', 'ElokNurHamdana@polinema.ac.id', '82184', 'AKTIF'),
-	('P4', 'L3', '2241720005', 'vunkg', 'Vunky Himawan', 'VunkyHimawan@polinema.ac.id', '82185', 'AKTIF'),
-	('P5', 'L1', '502108400', 'awoon', 'Anggi Putra Woon', 'AnggiPutraWoon@polinema.ac.id', '82186', 'AKTIF'),
-	('P6', 'L1', '502108401', 'sjadi', 'Sujadi', 'Sujadi@polinema.ac.id', '82187', 'AKTIF'),
-	('P7', 'L3', '2241720192', 'achrhan', 'Achmad Raihan', 'AchmadRaihan@polinema.ac.id', '82188', 'AKTIF'),
-	('P8', 'L1', '502108402', 'atmnugh', 'Dwi Atmo Nugroho', 'DwiAtmoNugroho@polinema.ac.id', '82189', 'AKTIF');
+INSERT INTO Pengguna (ID_Pengguna, ID_Level, Username, Password, Nama, Email, Nomor_HP, Foto, Status) VALUES
+	('P1', 'L2', '404079101', 'adei', 'Ade Ismail', 'AdeIsmail@polinema.ac.id', '82182', CAST('' AS VARBINARY(50)), 'AKTIF'),
+	('P2', 'L3', '2241720220', 'putraz', 'Putra Zakaria', 'PutraZakaria@polinema.ac.id', '82183', CAST('' AS VARBINARY(50)), 'TIDAK AKTIF'),
+	('P3', 'L2', '702108601', 'eloks', 'Elok Nur Hamdana', 'ElokNurHamdana@polinema.ac.id', '82184', CAST('' AS VARBINARY(50)), 'AKTIF'),
+	('P4', 'L3', '2241720005', 'vunkg', 'Vunky Himawan', 'VunkyHimawan@polinema.ac.id', '82185', CAST('' AS VARBINARY(50)), 'AKTIF'),
+	('P5', 'L1', '502108400', 'awoon', 'Anggi Putra Woon', 'AnggiPutraWoon@polinema.ac.id', '82186', CAST('' AS VARBINARY(50)), 'AKTIF'),
+	('P6', 'L1', '502108401', 'sjadi', 'Sujadi', 'Sujadi@polinema.ac.id', '82187', CAST('' AS VARBINARY(50)), 'AKTIF'),
+	('P7', 'L3', '2241720192', 'achrhan', 'Achmad Raihan', 'AchmadRaihan@polinema.ac.id', '82188', CAST('' AS VARBINARY(50)), 'AKTIF'),
+	('P8', 'L1', '502108402', 'atmnugh', 'Dwi Atmo Nugroho', 'DwiAtmoNugroho@polinema.ac.id', '82189', CAST('' AS VARBINARY(50)), 'AKTIF');
 
 INSERT INTO OTP (ID_OTP, ID_Pengguna, Kode, Expired) VALUES
 	('O1', 'P1', 'SAD12', '2023-10-30 12:00:00'),
@@ -33,14 +33,14 @@ INSERT INTO OTP (ID_OTP, ID_Pengguna, Kode, Expired) VALUES
 	('O4', 'P4', 'DETE43', '2023-11-10 10:00:00'),
 	('O5', 'P7', 'JJD083', '2023-11-10 11:00:00');
 
-INSERT INTO Inventaris (ID_Inventaris, Nama, Stok, ID_Kategori, Asal, Deskripsi ) VALUES
-	('I1', 'Spidol', '10', 'K3', 'Hibah', '-'),
-	('I2', 'Penghapus', '7', 'K3', 'Beli', '-'),
+INSERT INTO Inventaris (ID_Inventaris, Nama, Stok, ID_Kategori, Asal, Deskripsi, Gambar ) VALUES
+	('I1', 'Spidol', '10', 'K3', 'Hibah', '-', CAST('' AS VARBINARY(50))),
+	('I2', 'Penghapus', '7', 'K3', 'Beli', '-', CAST('' AS VARBINARY(50))),
 	('I3', 'Tang Crimping', '14', 'K1', 'Hibah'),
-	('I4', 'Obeng', '16', 'K1', 'Beli', '-'),
-	('I5', 'Konektor Proyektor', '30', 'K2', 'Hibah', '-'),
-	('I6', 'Keyboard', '17', 'K2', 'Beli', '-'),
-	('I7', 'Mouse', '24', 'K2', 'Hibah', '-');
+	('I4', 'Obeng', '16', 'K1', 'Beli', '-', CAST('' AS VARBINARY(50))),
+	('I5', 'Konektor Proyektor', '30', 'K2', 'Hibah', '-', CAST('' AS VARBINARY(50))),
+	('I6', 'Keyboard', '17', 'K2', 'Beli', '-', CAST('' AS VARBINARY(50))),
+	('I7', 'Mouse', '24', 'K2', 'Hibah', '-', CAST('' AS VARBINARY(50)));
 
 INSERT INTO Maintainer (ID_Maintainer, Nama) VALUES
 	('M1', 'Mas Woon'),
@@ -56,9 +56,9 @@ INSERT INTO MaintainerInventaris (ID_Inventaris, ID_Maintainer) VALUES
 	('I6', 'M2'),
 	('I7', 'M3');
 
-INSERT INTO Transaksi (ID_Transaksi, ID_Pengguna, ID_Admin, ID_Status, StartDate, EndDate, Deskripsi_Keperluan, Pesan) VALUES
-	('T1', 'P1', 'P6', 'S5', '2023-11-11 10:00:00', '2023-11-14 10:00:00', 'Keperluan Mengajar', 'Silahkan Ambil Barang di ruang teknisi Lantai 7'),
-	('T2', 'P4', 'P8', 'S6', '2023-11-13 10:00:00', '2023-11-15 10:00:00', 'Keperluan Belajar Mandiri', 'Peminjaman Dibatalkan oleh peminjam');
+INSERT INTO Transaksi (ID_Transaksi, ID_Pengguna, ID_Admin, ID_Status, StartDate, EndDate, Deskripsi_Keperluan, Jaminan, Pesan) VALUES
+	('T1', 'P1', 'P6', 'S5', '2023-11-11 10:00:00', '2023-11-14 10:00:00', 'Keperluan Mengajar', CAST('' AS VARBINARY(50)), 'Silahkan Ambil Barang di ruang teknisi Lantai 7'),
+	('T2', 'P4', 'P8', 'S6', '2023-11-13 10:00:00', '2023-11-15 10:00:00', 'Keperluan Belajar Mandiri', CAST('' AS VARBINARY(50)), 'Peminjaman Dibatalkan oleh peminjam');
 
 INSERT INTO DetailTransaksi (ID_DetailTrc, ID_Transaksi, ID_Inventaris, Jumlah) VALUES
 	('DT1', 'T1', 'I3', '10'),
