5c45ab90376ad7d7a801b5a1f2944e82f86790f9
Vunky Himawan <vunkyhimawan19@gmail.com>
2023-11-28 19:20:57 +0700

    feat(user-dashboard): create user dashboard view
    - Design and implemented the user dashboard UI
    - Note: Mobile UI has not been created yet so this is only a UI for desktop (non-responsive)
    - TODO: Complete and finalize the dashboard view

--- app/View/footer.php
+++ app/View/footer.php
@@ -1,3 +1,170 @@
+</main>
+</div>
 
+<script>
+  feather.replace();
+</script>
+
+<script>
+  $(document).ready(function() {
+    /* This function is used to update the state of the sidebar */
+    $(".sidebar-btn").on("click", () => {
+      $(".sidebar-btn").toggleClass("sidebar-btn-rotate");
+      if ($(".sidebar-btn").hasClass("sidebar-btn-rotate")) {
+        $(".sidebar").css("width", "10vw");
+        $(".main-container").css("width", "90vw");
+        $(".logo").addClass("hidden");
+        $(".text-menu").addClass("hidden");
+      } else {
+        $(".sidebar").css("width", "20vw");
+        $(".main-container").css("width", "80vw");
+        $(".logo").removeClass("hidden");
+        $(".text-menu").removeClass("hidden");
+      }
+    });
+
+    /* This function is used to update the state of the filter */
+    $('.btn-filter').on('click', () => {
+      if ($('.btn-sort').hasClass('hidden')) {
+        $('.filter-list').toggleClass('hidden');
+      } else {
+        $('.sort-list').addClass('hidden');
+        $('.filter-list').toggleClass('hidden');
+      }
+    });
+
+    /* This function is used to update the state of the sort */
+    $('.btn-sort').on('click', () => {
+      if ($('.btn-filter').hasClass('hidden')) {
+        $('.sort-list').toggleClass('hidden');
+      } else {
+        $('.filter-list').addClass('hidden');
+        $('.sort-list').toggleClass('hidden');
+      }
+    });
+
+    /* This function is used to update the state of the hamburger menu */
+    $('.hamburger-menu').on('click', () => {
+      if ($('.line-1').hasClass('line-1-rotate')) {
+        $('.menu').css('transform', 'translateY(-100%)');
+        $('.line-1').removeClass('line-1-rotate');
+        $('.line-1').attr('y', '11')
+        $('.line-1').attr('x', '0')
+        $('.line-2').removeClass('line-2-rotate');
+        $('.line-2').attr('y', '16')
+        $('.line-2').attr('x', '0')
+      } else {
+        $('.menu').css('transform', 'translateY(0)');
+        $('.line-1').addClass('line-1-rotate');
+        $('.line-1').attr('y', '-1')
+        $('.line-1').attr('x', '2')
+        $('.line-2').addClass('line-2-rotate');
+        $('.line-2').attr('x', '-15')
+      }
+    });
+  });
+</script>
+
+<!-- This script is used to update the state of the loan button -->
+<script>
+  $(document).ready(function() {
+    let total = 0;
+
+    /* When the user click on the pinjam button, increment the counter and replace the pinjam button with a counter */
+    $(document).on('click', '.inventory-item-button', function() {
+      let counter = 1;
+      total++;
+
+      $(this).replaceWith(`
+      <div class="counter-container w-100 d-flex justify-content-between">
+        <label for="pinjam" class="btn-counter btn-counter-min btn btn-primary">-</label>
+        <input id="pinjam" type="text" value="${counter}" class="counter-input w-50 rounded bg-dark-subtle">
+        <label for="pinjam" class="btn-counter btn-counter-plus btn btn-primary">+</label>
+      </div>
+    `);
+
+      updateLoanButtonState();
+    });
+
+    /* When the user clicks on the button, increment the counter */
+    $(document).on('click', '.btn-counter-plus', function() {
+      let counter = parseInt($(this).prev().val()) + 1;
+      total++;
+
+      $(this).prev().val(counter);
+
+      updateLoanButtonState();
+    });
+
+    /* when the user clicks on the button, decrement the counter */
+    $(document).on('click', '.btn-counter-min', function() {
+      let counter = parseInt($(this).next().val()) - 1;
+      total--;
+
+      if (counter > 0) {
+        $(this).next().val(counter);
+      } else {
+        $(this).closest('.counter-container').replaceWith(`
+        <button class="inventory-item-button btn btn-primary w-100" type="button">
+          Pinjam
+        </button>
+      `);
+      }
+
+      updateLoanButtonState();
+    });
+
+    /* This function is used to update the state of the loan button */
+    function updateLoanButtonState() {
+      if (total > 0) {
+        $('.loan-button').attr('disabled', false);
+        $('.loan-button').text(`Pinjam (${total})`);
+      } else {
+        $('.loan-button').attr('disabled', true);
+        $('.loan-button').text(`Pinjam (${total})`);
+      }
+    }
+
+    updateLoanButtonState();
+  })
+</script>
+
+<!-- This script is used to update the calendar -->
+<script>
+  $(document).ready(function() {
+    (() => {
+      const date = new Date();
+
+      const months = [
+        "Januari",
+        "Februari",
+        "Maret",
+        "April",
+        "Mei",
+        "Juni",
+        "Juli",
+        "Agustus",
+        "September",
+        "Oktober",
+        "November",
+        "Desember",
+      ];
+
+      $(".calendar-month").text(`${months[date.getMonth()]} ${date.getFullYear()}`);
+
+      const firstDay = getFirstDay(date.getFullYear(), date.getMonth());
+      const lastDay = getLastDay(date.getFullYear(), date.getMonth());
+
+      function getFirstDay(year, month) {
+        return new Date(year, month, 1);
+      }
+
+      function getLastDay(year, month) {
+        return new Date(year, month + 1, 0);
+      }
+    })();
+  })
+</script>
 </body>
+
 </html>
--- app/View/header.php
+++ app/View/header.php
@@ -1,8 +1,97 @@
+<?php
+$current_page_url = $_SERVER['REQUEST_URI'];
+
+$menu_items = [
+    'dashboard' => '/dashboard',
+    'peminjaman' => '/peminjaman',
+    'riwayat' => '/riwayat',
+];
+
+function active_page($current_page, $target)
+{
+    return $current_page === $target;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Peminjaman</title>
+    <link rel="stylesheet" href="./public/assets/css/style.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <script src="https://unpkg.com/feather-icons"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 </head>
-<body>
+
+<body style="background-color: #ececec">
+    <div class="d-flex vw-100 h-auto">
+        <!-- Side Bar -->
+        <div class="sidebar duration-300 d-flex flex-column gap-5 bg-white container-fluid p-4 vh-100 d-none d-xl-flex">
+            <div class="d-flex gap-2 justify-content-between align-items-center">
+                <div class="d-flex gap-2">
+                    <div class="rounded-circle" style="width: 1.5rem; height: 1.5rem; background-color: #0a60a4"></div>
+                    <div class="rounded-circle" style="width: 1.5rem; height: 1.5rem; background-color: #ffa500"></div>
+                    <div class="rounded-circle" style="width: 1.5rem; height: 1.5rem; background-color: #e3f2f9"></div>
+                </div>
+                <div class="sidebar-btn" style="cursor: pointer">
+                    <i data-feather="chevrons-right"></i>
+                </div>
+            </div>
+            <div class="logo duration-500 w-100">
+                <img src="./public/assets/images/polinema-logo.png" alt="" class="object-fit-cover ratio-16x9 w-100" />
+            </div>
+            <div>
+                <ul class="d-flex flex-column row-gap-4" style="list-style: none; padding: 0">
+                    <li class="d-flex gap-2 p-2 rounded-2 <?php echo active_page($current_page_url, $menu_items['dashboard']) ? 'bg-primary text-white' : 'text-dark' ?>">
+                        <i data-feather="grid"></i>
+                        <a href="/dashboard" class="text-menu text-decoration-none <?php echo active_page($current_page_url, $menu_items['dashboard']) ? 'text-white' : 'text-dark' ?>">Dashboard</a>
+                    </li>
+                    <li class="d-flex gap-2 p-2 rounded-2 <?php echo active_page($current_page_url, $menu_items['peminjaman']) ? 'bg-primary text-white' : 'text-dark' ?>">
+                        <i data-feather="shopping-cart"></i>
+                        <a href="/peminjaman" class="text-menu text-decoration-none <?php echo active_page($current_page_url, $menu_items['peminjaman']) ? 'text-white' : 'text-dark' ?>">Peminjaman</a>
+                    </li>
+                    <li class="d-flex gap-2 p-2 rounded-2 <?php echo active_page($current_page_url, $menu_items['riwayat']) ? 'text-white' : 'text-dark' ?>">
+                        <i data-feather="clock"></i>
+                        <a href="/riwayat" class="text-menu text-decoration-none <?php echo active_page($current_page_url, $menu_items['riwayat']) ? 'text-white' : 'text-dark' ?>">History</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <main class="container-fluid main-container duration-300 h-auto d-flex flex-column justify-content-between vh-100">
+
+            <!-- Header -->
+            <header class="d-flex bg-white p-3 justify-content-between duration-300 z-3">
+                <div class="header-logo">
+                    <img src="./public/assets/images/logo-tulisan.png" alt="" class="w-100 object-fit-cover ratio-16x9" />
+                </div>
+                <div class="d-flex justify-content-center align-items-center gap-3">
+                    <div>
+                        <i data-feather="search"></i>
+                    </div>
+                    <div class="d-none d-xl-block">
+                        <i data-feather="mail"></i>
+                    </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="30" height="30" class="hamburger-menu d-xl-none">
+                        <rect y="11" width="30" height="2" rx="1" class="line-1 duration-300" />
+                        <rect y="16" width="30" height="2" rx="1" class="line-2 duration-300" />
+                    </svg>
+
+                    <!-- Profile -->
+                    <div class="d-flex justify-content-center align-items-center gap-2 rounded-5 p-2 d-none d-xl-flex">
+                        <div style="width: 3rem; height: 3rem" class="rounded-circle position-relative ">
+                            <div class="position-absolute -top-0 end-0 bg-success rounded-circle" style="width: 1rem; height: 1rem"></div>
+                            <div class="rounded-circle overflow-hidden w-100 h-100">
+                                <img src="https://source.unsplash.com/random/900×700/?potrait" alt="" class="object-fit-cover ratio-1x1 w-100 h-100" />
+                            </div>
+                        </div>
+                        <div class="d-flex flex-column justify-content-between">
+                            <strong class="name" style="font-size: 0.7rem">Putra Z.M</strong>
+                            <p class="m-0 text-body-tertiary" style="font-size: 0.7rem">Student</p>
+                        </div>
+                    </div>
+                </div>
+            </header>
