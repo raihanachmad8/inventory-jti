ee2e44123ba09b9aea19e868a005ce04bba0644c
Vunky Himawan <vunkyhimawan19@gmail.com>
2023-12-01 08:45:05 +0700

    feat(admin-inventory): implement desktop view and add icons
    - Designed and implemented the desktop view for the admin inventory feature
    - Added new icons for enchnced visual representation
    - NOTE: Mobile view is pending, and responsiveness is not finalized
    - TODO: Complete mobile responsiveness and finalize the design

--- public/route.php
+++ public/route.php
@@ -9,6 +9,7 @@ require_once __DIR__ . '/../app/Controllers/PeminjamanController.php';
 require_once __DIR__ . '/../app/Controllers/RiwayatController.php';
 require_once __DIR__ . '/../app/Controllers/DashboardAdminController.php';
 require_once __DIR__ . '/../app/Controllers/DataPeminjamanAdminController.php';
+require_once __DIR__ . '/../app/Controllers/InventarisirController.php';
 
 
 Router::route('GET', '/', [HomeController::class, 'index']);
@@ -18,6 +19,6 @@ Router::get('/peminjaman', [PeminjamanController::class, 'index']);
 Router::get('/riwayat', [RiwayatController::class, 'index']);
 Router::get('/admin', [DashboardAdminController::class, 'index']);
 Router::get('/admin/data-peminjaman', [DataPeminjamanAdminController::class, 'index']);
-
+Router::get('/admin/inventarisir', [InventarisirController::class, 'index']);
 
 Router::run();
