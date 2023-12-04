7883799f08837b2a4380a905dc380c68fa96b67b
Vunky Himawan <vunkyhimawan19@gmail.com>
2023-12-05 00:27:04 +0700

    feat(landing): make landing page view responsive
    - Add responsive design to the landing page view for better user experience

--- app/View/footer.php
+++ app/View/footer.php
@@ -1,3 +1,36 @@
+<script>
+  const hamburgerNavbar = document.querySelector(".hamburger-nav");
+  const navMenu_el = document.querySelector('.nav-menu');
+  const navLink_el = document.querySelectorAll('.nav-link');
+
+  hamburgerNavbar.addEventListener('click', () => {
+    navMenu_el.classList.toggle('nav-menu-active');
+    navLink_el.forEach(link => {
+      link.addEventListener('click', () => {
+        navMenu_el.classList.remove('nav-menu-active');
+      })
+    })
+  })
+
+  const navbar_el = document.querySelector('.nav');
+
+  let lastScrollTop = 0;
+  window.addEventListener('scroll', () => {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+      if (navbar_el.classList.contains('nav-scroll-down')) {
+        navbar_el.classList.replace('nav-scroll-down', 'nav-scroll-up');
+      } else {
+        navbar_el.classList.add('nav-scroll-up');
+      }
+    } else {
+      navbar_el.classList.replace('nav-scroll-up', 'nav-scroll-down');
+    }
+    lastScrollTop = scrollTop;
+  })
+</script>
+
 
 </body>
+
 </html>
--- public/route.php
+++ public/route.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/../app/App/View.php';
 require_once __DIR__ . '/../app/Controllers/HomeController.php';
 
 
-Router::route('GET','/', [HomeController::class, 'index']);
+Router::route('GET', '/', [HomeController::class, 'index']);
 Router::get('/about', [HomeController::class, 'about']);
 
 
