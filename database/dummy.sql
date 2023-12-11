e127776d16a5729dd9f346b1c110b8ca2e444409
PutraZakaria <117093982+PutraZakaria@users.noreply.github.com>
2023-12-11 13:49:53 +0700

    Feature/Database

--- database/dummy.sql
+++ database/dummy.sql
@@ -19,22 +19,13 @@ INSERT INTO Kategori (ID_Kategori, Nama) VALUES
 	('K3', 'ATK');
 
 INSERT INTO Pengguna (ID_Pengguna, ID_Level, `Nomor_Identitas`, Password, Nama, Email, Nomor_HP, Foto, `Status`) VALUES
-	('P1', 'L2', '404079101', 'adei', 'Ade Ismail', 'AdeIsmail@polinema.ac.id', '82182', 'profile1.png', 'Aktif'),
-	('P2', 'L3', '2241720220', 'putraz', 'Putra Zakaria', 'PutraZakaria@polinema.ac.id', '82183', 'profile2.png', 'Aktif'),
-	('P3', 'L2', '702108601', 'eloks', 'Elok Nur Hamdana', 'ElokNurHamdana@polinema.ac.id', '82184', 'profile3.png', 'Aktif'),
-	('P4', 'L3', '2241720005', 'vunkg', 'Vunky Himawan', 'VunkyHimawan@polinema.ac.id', '82185', 'profile4.png', 'Aktif'),
-	('P5', 'L2', '502108400', 'awoon', 'Anggi Putra Woon', 'AnggiPutraWoon@polinema.ac.id', '82186', 'profile5.png', 'Aktif'),
-	('P6', 'L2', '502108401', 'sjadi', 'Sujadi', 'Sujadi@polinema.ac.id', '82187', 'profile6.png', 'Aktif'),
-	('P7', 'L3', '2241720192', 'achrhan', 'Achmad Raihan', 'AchmadRaihan@polinema.ac.id', '82188', 'profile7.png', 'Aktif'),
-	('P8', 'L2', '502108402', 'atmnugh', 'Dwi Atmo Nugroho', 'DwiAtmoNugroho@polinema.ac.id', '82189', 'profile8.png', 'Aktif'),
-	('P9', 'L1', '1', 'admin', 'Admin', '-', '-', '-', 'Aktif');
+	('P1', 'L1', '1', 'Admin', 'Admin', '-', '-', '-', 'Aktif'),
+	('P2', 'L2', '0702108601', 'Dosen', 'Elok Nur Hamdana', 'ElokNurHamdana@polinema.ac.id', '82184', 'ProfileP2.png', 'Aktif'),
+	('P3', 'L3', '2241720220', 'Mahasiswa', 'Putra Zakaria Muzaki', 'PutraZakaria@polinema.ac.id', '82183', 'ProfileP3.png', 'Aktif');
 
 INSERT INTO OTP (ID_OTP, ID_Pengguna, Kode, Expired) VALUES
-	('O1', 'P1', 'SAD12', '2023-10-30 12:00:00'),
-	('O2', 'P2', 'DAQW23', '2023-10-31 0:00:00'),
-	('O3', 'P3', 'DFE421', '2023-11-03 15:00:00'),
-	('O4', 'P4', 'DETE43', '2023-11-10 10:00:00'),
-	('O5', 'P7', 'JJD083', '2023-11-10 11:00:00');
+	('O1', 'P2', 'SAD12', '2023-10-30 12:00:00'),
+	('O2', 'P3', 'DAQW23', '2023-10-31 0:00:00');
 
 INSERT INTO Inventaris (ID_Inventaris, Nama, Stok, ID_Kategori, Asal, Deskripsi, Gambar ) VALUES
 	('I1', 'Spidol', '10', 'K3', 'Hibah', '-', 'inventaris1.png'),
@@ -43,11 +34,8 @@ INSERT INTO Inventaris (ID_Inventaris, Nama, Stok, ID_Kategori, Asal, Deskripsi,
 	('I4', 'Obeng', '16', 'K1', 'Beli', '-', 'inventaris4.png'),
 	('I5', 'Konektor Proyektor', '30', 'K2', 'Hibah', '-', 'inventaris5.png'),
 	('I6', 'Keyboard', '17', 'K2', 'Beli', '-', 'inventaris6.png'),
-	('I7', 'Mouse', '24', 'K2', 'Hibah', '-', 'inventaris7.png');
-
-INSERT INTO Inventaris (ID_Inventaris, Nama, Stok, ID_Kategori, Asal, Deskripsi, Gambar ) VALUES
+	('I7', 'Mouse', '24', 'K2', 'Hibah', '-', 'inventaris7.png'),
 	('I8', 'Monitor', '10', 'K2', 'Hibah', '-', 'inventaris8.png');
-
 
 INSERT INTO Maintainer (ID_Maintainer, Nama) VALUES
 	('M1', 'Mas Woon'),
@@ -61,13 +49,52 @@ INSERT INTO MaintainerInventaris (ID_Inventaris, ID_Maintainer) VALUES
 	('I4', 'M3'),
 	('I5', 'M1'),
 	('I6', 'M2'),
-	('I7', 'M3');
+	('I7', 'M3'),
+	('I8', 'M3');
 
-INSERT INTO Transaksi (ID_Transaksi, ID_Pengguna, ID_Admin, ID_Status, StartDate, EndDate, Deskripsi_Keperluan, Jaminan, Pesan) VALUES
-	('T1', 'P1', 'P9', 'S5', '2023-11-11 10:00:00', '2023-11-14 10:00:00', 'Keperluan Mengajar', '', 'Silahkan Ambil Barang di ruang teknisi Lantai 7'),
-	('T2', 'P4', 'P9', 'S6', '2023-11-13 10:00:00', '2023-11-15 10:00:00', 'Keperluan Belajar Mandiri', 'jaminan1.png', 'Peminjaman Dibatalkan oleh peminjam');
+INSERT INTO tableName (ID_Transaksi, ID_Pengguna, ID_Admin, ID_Status, StartDate, EndDate, Deskripsi_Keperluan, Jaminan, Pesan) VALUES
+	('T1', 'P2', 'P1', 'S5', '2023-11-01 8:00:00', '2023-11-05 15:00:00', 'Perlu untuk acara seminar', '-', 'Silahkan diambil diruang admin lt.6'),
+	('T2', 'P3', 'P1', 'S5', '2023-11-02 9:00:00', '2023-11-08 15:00:00', 'Untuk keperluan riset', 'JaminanT2.png', 'Silahkan diambil diruang teknisi lt.7'),
+	('T3', 'P2', 'P1', 'S2', '2023-11-05 10:00:00', '2023-11-15 15:00:00', 'Acara workshop', '-', 'Silahkan diambil diruang teknisi lt.5'),
+	('T4', 'P3', 'P1', 'S5', '2023-11-10 11:00:00', '2023-11-20 15:00:00', 'Untuk keperluan presentasi', 'JaminanT4.png', 'Silahkan diambil diruang admin lt.6'),
+	('T5', 'P2', 'P1', 'S5', '2023-11-15 12:00:00', '2023-11-25 15:00:00', 'Acara pelatihan', '-', 'Silahkan diambil diruang teknisi lt.7'),
+	('T6', 'P3', 'P1', 'S6', '2023-11-20 13:00:00', '2023-11-30 15:00:00', 'Untuk tugas kuliah', 'JaminanT6.png', 'Silahkan diambil diruang admin lt.6'),
+	('T7', 'P3', 'P1', 'S4', '2023-11-30 15:00:00', '2023-12-10 15:00:00', 'Untuk presentasi akhir', 'JaminanT7.png', 'Silahkan diambil diruang teknisi lt.7'),
+	('T8', 'P2', 'P1', 'S7', '2023-11-25 14:00:00', '2023-12-05 15:00:00', 'Acara seminar ', '-', 'Silahkan diambil diruang teknisi lt.5'),
+	('T9', 'P2', 'P1', 'S4', '2023-12-01 16:00:00', '2023-12-12 15:00:00', 'Acara workshop akhir tahun', '-', 'Silahkan diambil diruang admin lt. 6'),
+	('T10', 'P3', 'P1', 'S3', '2023-12-05 17:00:00', '2023-12-15 15:00:00', 'Untuk kegiatan UKM', 'JaminanT8.png', 'Silahkan diambil diruang teknisi lt.7'),
+	('T11', 'P3', 'P1', 'S1', '2023-12-06 17:00:00', '2023-12-15 15:00:00', 'Untuk mengerjakan tugas akhir', 'JaminanT9.png', 'Silahkan diambil diruang teknisi lt.5');
 
 INSERT INTO DetailTransaksi (ID_DetailTrc, ID_Transaksi, ID_Inventaris, Jumlah, Kondisi) VALUES
-	('DT1', 'T1', 'I3', '10', 'Normal'),
-	('DT2', 'T1', 'I4', '10', 'Rusak'),
-	('DT3', 'T1', 'I5', '1', 'Normal');
+	('DT1', 'T1', 'I1', '2', 'Normal'),
+	('DT2', 'T1', 'I3', '3', 'Normal'),
+	('DT3', 'T1', 'I2', '1', 'Normal'),
+	('DT4', 'T2', 'I7', '5', 'Normal'),
+	('DT5', 'T2', 'I8', '2', 'Normal'),
+	('DT6', 'T2', 'I4', '2', 'Normal'),
+	('DT7', 'T3', 'I6', '3', 'Normal'),
+	('DT8', 'T3', 'I2', '4', 'Normal'),
+	('DT9', 'T3', 'I5', '1', 'Normal'),
+	('DT10', 'T4', 'I4', '2', 'Normal'),
+	('DT11', 'T4', 'I6', '3', 'Normal'),
+	('DT12', 'T4', 'I1', '2', 'Normal'),
+	('DT13', 'T5', 'I3', '1', 'Normal'),
+	('DT14', 'T5', 'I1', '5', 'Normal'),
+	('DT15', 'T5', 'I4', '2', 'Normal'),
+	('DT16', 'T6', 'I2', '1', 'Normal'),
+	('DT17', 'T6', 'I7', '4', 'Normal'),
+	('DT18', 'T6', 'I5', '2', 'Normal'),
+	('DT19', 'T7', 'I4', '3', 'Rusak'),
+	('DT20', 'T7', 'I6', '2', 'Hilang'),
+	('DT21', 'T7', 'I7', '3', 'Normal'),
+	('DT22', 'T8', 'I2', '2', 'Normal'),
+	('DT23', 'T8', 'I1', '2', 'Normal'),
+	('DT24', 'T8', 'I3', '3', 'Normal'),
+	('DT25', 'T9', 'I2', '4', 'Normal'),
+	('DT26', 'T9', 'I4', '1', 'Normal'),
+	('DT27', 'T9', 'I5', '1', 'Normal'),
+	('DT28', 'T10', 'I4', '2', 'Normal'),
+	('DT29', 'T10', 'I6', '3', 'Normal'),
+	('DT30', 'T10', 'I7', '3', 'Normal'),
+	('DT31', 'T11', 'I4', '1', 'Normal'),
+	('DT32', 'T11', 'I3', '1', 'Normal');
